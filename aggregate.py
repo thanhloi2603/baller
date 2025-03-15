1e132ccf39390b9bc39cb39bdebbc253cbe6c5e0
loidang <loi.dangthanh@gmail.com>
2025-03-15 18:41:56 +0700

    input the xlsx file

--- aggregate.py
+++ aggregate.py
@@ -1,10 +1,11 @@
 import os
+import sys
 from sheet_converter import convert_xlsx_to_csv, TransactionManager
 from user import Usermanager
 
 
 def setup():
-    input_file_path = 'input_sheets/SaoKeTK_29122024_31012025.xlsx'
+    input_file_path = sys.argv[1]
     csv_file_path = os.path.join(os.path.dirname(input_file_path), os.path.splitext(os.path.basename(input_file_path))[0] + '.csv')
     convert_xlsx_to_csv(input_file_path, csv_file_path)
     um = Usermanager()
